0343370d430f74df217b15399473314b3e90b7ac
Ismail Omodara <omodara145@gmail.com>
2020-12-18 04:29:29 +0100

    Added prop type check for props - book in Book.js

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const bookState = ['currentlyReading', 'wantToRead', 'read', 'none']
 const bookStateTitles = {
@@ -8,35 +9,41 @@ const bookStateTitles = {
   none: 'None'
 }
 
-const Book = (props) => {
-  const book = props.book;
-  const bookImage = book.imageLinks && book.imageLinks.thumbnail
-  const bookTitle = book.title && props.book.title
-  const bookAuthors = book.authors && book.authors.join(', ')
+class Book extends React.Component  {
+  static propTypes = {
+    book: PropTypes.object.isRequired
+  }
 
-  return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover"
-             style={{
-               width: 128,
-               height: 193,
-               backgroundImage: `url('${bookImage}')` }}></div>
-          <div className="book-shelf-changer">
-            <select value={props.shelf} onChange={(event) => props.updateShelf(props.book, event.target.value)}>
-              <option value="move" disabled>Move to...</option>
-              {bookState.map((state, index) =>
-                <option
-                  key={index}
-                  value={state}
-                  >{ bookStateTitles[state] }</option> )}
-            </select>
+  render() {
+    const book = this.props.book;
+    const bookImage = book.imageLinks && book.imageLinks.thumbnail
+    const bookTitle = book.title && book.title
+    const bookAuthors = book.authors && book.authors.join(', ')
+
+    return (
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover"
+                 style={{
+                   width: 128,
+                   height: 193,
+                   backgroundImage: `url('${bookImage}')` }}></div>
+            <div className="book-shelf-changer">
+              <select value={this.props.shelf} onChange={(event) => this.props.updateShelf(book, event.target.value)}>
+                <option value="move" disabled>Move to...</option>
+                {bookState.map((state, index) =>
+                    <option
+                        key={index}
+                        value={state}
+                    >{ bookStateTitles[state] }</option> )}
+              </select>
+            </div>
           </div>
+          <div className="book-title">{ bookTitle }</div>
+          <div className="book-authors">{ bookAuthors }</div>
         </div>
-        <div className="book-title">{ bookTitle }</div>
-        <div className="book-authors">{ bookAuthors }</div>
-      </div>
-  )
+    )
+  }
 }
 
 export default Book
